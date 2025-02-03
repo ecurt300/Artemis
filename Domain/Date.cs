1f242aefea5b705609eda5159a47662c9b50bc11
Edward Curtis <gamerguy1835@yahoo.com>
2025-02-03 15:12:19 -0500

    Code Commit

---
+++ Domain/Date.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Moon_Phase_App.Domain
+{
+    public class Date
+    {
+        public int Day, Month, Year;
+
+
+        public Date(int Day, int Month, int Year)
+        {
+            this.Day = Day;
+            this.Month = Month;
+            this.Year = Year;
+        }
+
+        public override string ToString()
+        {
+            return $"D/ {Day}/Month/{Month}/Year{Year} ";
+
+        }
+
+    }
+}
---
+++ Domain/Results.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moon_Phase_App.Domain
+{
+    public class Results
+    {
+        public string UserName { get; set; }
+
+        public Phase moonPhase { get; set; }
+
+        public Date moonDate { get; set; }
+
+
+
+        public Results(Phase moonPhase, Date Date, string userName)
+        {
+            this.moonPhase = moonPhase;
+            UserName = userName;
+            moonDate = Date;
+        }
+
+        public override string ToString()
+        {
+            return $" Name of User {UserName}/ Phase of Moon: {moonPhase}/Date of Moon Result {moonDate}";
+        }
+
+    }
+}
