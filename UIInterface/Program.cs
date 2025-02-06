79c976c5a4d912293a90524a7989fd6e37211c53
Edward Curtis <gamerguy1835@yahoo.com>
2025-02-06 12:48:43 -0500

    Correct Repository from Commit 1

---
+++ UIInterface/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Artemis;
+using MoonData.Data;
+using MoonData.Domain;
+using Utilities;
+bool canQuite = false;
+MoonFakeDatabase moonFakeDatabase = new MoonFakeDatabase();
+   /*
+   * Quick Values such for some nonsense dates for testing
+   * 
+   */
+moonFakeDatabase.AddToFakeDatabase(new Moon(Phase.WolfMoon, new Date(27, 10, 1948, 1), 0));
+moonFakeDatabase.AddToFakeDatabase(new Moon(Phase.WormMoon, new Date(27, 10, 1978, 2), 1));
+moonFakeDatabase.AddToFakeDatabase(new Moon(Phase.BuckMoon, new Date(27, 10, 1998, 3), 2));
+while (!canQuite)
+{
+    
+   
+    CLIUI.Instance().WriteInput("Hello Artemis");
+    CLIUI.Instance().WriteInput("Press 1 to quite");
+    Console.WriteLine(moonFakeDatabase.ToString());
+
+   
+    canQuite = CLIUI.Instance().ContextChoose();
+    
+    
+  
+    
+}
+
---
+++ Utilities/CLIUI.cs
@@ -0,0 +1,76 @@
+﻿
+
+
+namespace Utilities
+{
+    public class CLIUI
+    {
+        //Quick and Dirty Singleton
+
+        private static CLIUI? instance { get; set; }
+
+        public static CLIUI Instance()
+        {
+            if (instance == null)
+            {
+                instance = new CLIUI();
+            }
+            return instance;
+        }
+
+
+        //Some Basic Console Helper Functions for testing EFcore and the logic of the App.
+        public bool ContextChoose()
+        {
+
+            string? input = Console.ReadLine()?.Trim().ToLower();
+            var stringInput = 0;
+            bool canParse = int.TryParse(input, out stringInput);
+
+            if (stringInput == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+        
+        public void WriteInput(string input)
+        {
+            var textToDisplay = input.Trim();
+            Console.WriteLine(textToDisplay);
+        }
+        public string GetInput(string input)
+        {
+            var stringInput = input.Trim().ToLower();
+
+            return stringInput;
+        }
+        public int GetInputInteger()
+        {
+            string? input = Console.ReadLine()?.Trim().ToLower();
+            var stringInput = 0;
+            bool canParse = int.TryParse(input,out stringInput);
+            
+
+            return stringInput;
+        }
+        public float GetFloatInput()
+        {
+            string? input = Console.ReadLine()?.Trim().ToLower();
+            var stringInput = 0f;
+            bool canParse = float.TryParse(input, out stringInput);
+
+
+            return stringInput;
+        }
+        public double GetDouble()
+        {
+            string? input = Console.ReadLine()?.Trim().ToLower();
+            var stringInput = 0d;
+            bool canParse = double.TryParse(input, out stringInput);
+
+
+            return stringInput;
+        }
+    }
+}
